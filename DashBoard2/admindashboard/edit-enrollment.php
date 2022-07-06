0a029b3289fef966861e0a1f3198c3ef4a34dd5d
dark web <petermbuthia112@gmail.com>
2022-07-06 11:35:46 +0300

    commited

--- DashBoard2/admindashboard/edit-enrollment.php
+++ DashBoard2/admindashboard/edit-enrollment.php
@@ -1,3 +1,23 @@
+<?php 
+   require_once('logics/dbconnection.php');
+   $fetchEnrollmentRecord= mysqli_query($conn, "SELECT signin WHERE no='".$_GET.['id']."'");
+   while($fetchEnrollmentRecord = mysqli_fetch_array($querystudent))
+   {
+    $firstname = $fetchEnrollmentRecord['firstname'];
+    $lastname = $fetchEnrollmentRecord['lastname'];
+   }
+   if ($conn)
+   {
+    echo "success";
+
+   }
+   else{
+    echo "unsuccessful";
+   }
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <?php require_once('includes/links.php')
@@ -18,7 +38,7 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header text-center bg-dark">
-                            <h2>Edit Students</h2>
+                            <h2>Edit Students<?php echo 'fullname' ?></h2>
                         </div>
                     </div>
                 </div>
